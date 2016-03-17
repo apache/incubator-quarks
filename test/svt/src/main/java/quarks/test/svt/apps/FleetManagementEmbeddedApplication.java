11cc85c81df6af0fc5aba5da62bc455bba6fdf79
May Wone <mnwone@gmail.com>
2016-03-16 17:20:34 -0700

    QUARKS-21 Add license text & 1 prologue

--- test/svt/src/main/java/quarks/test/svt/apps/FleetManagementEmbeddedApplication.java
+++ test/svt/src/main/java/quarks/test/svt/apps/FleetManagementEmbeddedApplication.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package quarks.test.svt.apps;
 
 import java.io.PrintWriter;
@@ -14,6 +31,30 @@ import quarks.topology.TStream;
 import quarks.topology.TWindow;
 import quarks.topology.Topology;
 
+/**
+ * These are sample Quarks Fleet Management applications that can be embedded in a motor vehicle. The Quarks 
+ * application reads in Global Positioning System (GPS) and On-board Diagnostics(OBD) tuples, which are then 
+ * analyzed in real-time.  
+ * 
+ * <p>
+ * The GPS application 1) logs the GPS coordinates, 2) filters for tuples where the speed exceeds a threshold,
+ * and 3) filters for tuples where the GPS coordinates is outside a geofence boundary, then publishes log/alert messages
+ * to a message broker such as Kafka or MQTT. 
+ * 
+ * <p> Optionally, centralized applications on servers can further manage the fleet of vehicles by 
+ * subscribing to the messages for all the vehicles. For example, a Quarks application can subscribe to these messages, which are converted into a 
+ * tuple flow for further analysis.  On a server, with more resources, Quarkscan correlate speeds with actual speed 
+ * limit signs at the GPS location, to assess the driver. Another example may be to visualize all the vehicles on a map. 
+ * These ideas are outside the scope of these sample applications. 
+ *  
+ * 
+ * TODO: Add OBD
+ * 
+ * Parameter (optional): console
+ * If specified, the Quarks development console's URL is written to file consoleUrl.txt. 
+ * Enter this URL into a browser to display the topology graph and metrics for 
+ * this application.
+ */
 
 public class FleetManagementEmbeddedApplication {   
 
