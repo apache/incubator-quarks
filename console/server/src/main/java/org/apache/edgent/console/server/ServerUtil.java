4234adb1410bda02e80229ea2de570966520bb0b
Christofer Dutz <christofer.dutz@codecentric.de>
2017-06-02 16:53:07 +0200

    Some additional fine-tuning: - Made the war-references use ${project.version} to make releasing easier - Configured the javadoc-plugin to not fail on errors - Moved plugin definitions from the pluginManagement section to the plugin section to automatically enable them

--- console/server/src/main/java/org/apache/edgent/console/server/ServerUtil.java
+++ console/server/src/main/java/org/apache/edgent/console/server/ServerUtil.java
@@ -19,17 +19,7 @@ under the License.
 
 package org.apache.edgent.console.server;
 
-import java.io.IOException;
 import java.io.File;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.security.ProtectionDomain;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ServerUtil {
 
@@ -37,27 +27,6 @@ public class ServerUtil {
 	 *  The public constructor of this utility class for use by the HttpServer class.
 	 */
     public ServerUtil() {
-
-    }
-    /**
-     * Returns the path to the jar file for this package
-     * @return a String representing the path to the jar file of this package
-     */
-    private String getPath() {
-        return getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-    }
-
-    /**
-     * Returns a file object representing the parent's parent directory of the jar file.
-     * @return a File object
-     */
-    private File getTopDirFilePath() {
-        String topDirProp = System.getProperty("edgent.test.top.dir.file.path");
-        if (topDirProp != null) {
-          return new File(topDirProp);
-        }
-        File jarFile = new File(getPath());
-        return jarFile.getParentFile().getParentFile().getParentFile();
     }
 
     /**
@@ -68,42 +37,4 @@ public class ServerUtil {
         return new File("target/war-resources/servlets.war");
     }
     
-    /**
-     * Looks for the absolute file path of the name of the warFileName argument
-     * @param warFileName the name of the war file to find the absolute path to
-     * @return the absolute path to the warFileName argument as a String
-     */
-    public String getAbsoluteWarFilePath(String warFileName) {
-        File warFilePath = getWarFilePath();
-        if (warFilePath != null) {
-        	File warFile = new File(warFilePath.getAbsolutePath() + "/" + warFileName);
-        	if (warFile.exists()) {        	
-        		return warFile.getAbsolutePath();
-        	} else {
-        		return "";
-        	}
-        }
-        else {
-            return "";
-        }
-    }
-    
-    /**
-     * Looks for the absolute file path of the name of the warFileName argument when running from Eclipse
-     * @param pDomain the ProtectionDomain to use to get the source's location
-     * @param warFileName the name of the war file to find the absolute path to
-     * @return the absolute path to the warFileName argument as a String
-     */
-    public String getEclipseWarFilePath(ProtectionDomain pDomain, String warFileName) {
-        URL location = pDomain.getCodeSource().getLocation();
-        File topEdgent = new File(location.getPath()).getParentFile().getParentFile();
-        File warFile = new File(topEdgent, "./target/java8/console/webapps/" +warFileName);
-        if (warFile.exists()) {
-        	return warFile.getAbsolutePath();
-        } else {
-        	return "";
-        }
-	
-    }
-
 }
